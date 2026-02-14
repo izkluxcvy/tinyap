e1ce7f27150937af29c7b5e1f3cf5672089ab721
izkluxcvy <pettyblue@proton.me>
2026-02-15 02:31:38 +0900

    use mimalloc for allocator

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ mod web;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+use mimalloc::MiMalloc;
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 #[tokio::main]
 async fn main() {
     cli::parse::parse().await;
