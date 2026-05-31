3a772f09be2e851ffa5379c556f923a3e99ef6ca
izkluxcvy <pettyblue@proton.me>
2026-06-01 00:33:50 +0900

    Fallback to proper Rfc3339 format
    Frontend can't parse "9999", invalid date format.
    Fallback to "9999-01-01T00:00:00Z", proper Rfc3339 date format.

--- src/back/utils.rs
+++ src/back/utils.rs
@@ -37,7 +37,7 @@ pub fn date_now_http_format() -> String {
 pub fn date_to_utc(date: &str) -> String {
     // let parsed = OffsetDateTime::parse(date, &Rfc3339).unwrap().to_utc();
     let Ok(parsed) = OffsetDateTime::parse(date, &Rfc3339) else {
-        return "9999".to_string();
+        return "9999-01-01T00:00:00Z".to_string();
     };
     parsed.format(&Rfc3339).unwrap()
 }
