02d1920d88dfd3c510f9c6e1a54d4c73b3012d5d
izkluxcvy <pettyblue@proton.me>
2026-08-27 03:11:49 +0900

    Improve memory usage on sqlite (2)

--- src/back/init.rs
+++ src/back/init.rs
@@ -139,6 +139,7 @@ async fn create_db_pool(conf: &HashMap<String, String>) -> sqlx::SqlitePool {
         .max_connections(3)
         .min_connections(1)
         .idle_timeout(None)
+        .max_lifetime(None)
         .connect_with(options)
         .await
         .unwrap()
